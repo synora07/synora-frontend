6d48c712b6262b6c80990aa05e23570803e0fa0f
synora07 <synorach@gmail.com>
2026-01-29 21:20:02 +0100

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+// ============================================
+// SYNORA VERSION 2.0 - KOMPONENTEN INDEX
+// Diese Datei exportiert alle Komponenten
+// ============================================
+
+export { default as VersionSelector } from './VersionSelector';
+export { default as WhyThisCard } from './WhyThisCard';
+export { default as WhyNotCard } from './WhyNotCard';
+export { default as TransparencyScore } from './TransparencyScore';
+export { default as ShopRating } from './ShopRating';
+export { default as PriceCompare } from './PriceCompare';
+export { default as HonestyBadge } from './HonestyBadge';
+export { default as PostPurchase } from './PostPurchase';
+export { default as ProblemHelper } from './ProblemHelper';
+export { default as SourcesDisplay } from './SourcesDisplay';
